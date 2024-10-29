0175fe381070911d5a1174aa7cbef297ca1ff512
Elvira Chala <chala.elvira1121@vu.cdu.edu.ua>
2024-10-29 11:46:46 +0200

    git init

---
+++ src/main/java/org/elvira/fooddeliveryorders/FoodDeliveryOrdersApplication.java
@@ -0,0 +1,13 @@
+package org.elvira.fooddeliveryorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryOrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodDeliveryOrdersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/elvira/fooddeliveryorders/FoodDeliveryOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elvira.fooddeliveryorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryOrdersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
